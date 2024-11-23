b2108f153c80596cb22a9000c3d2d0f0db75b9d5
lct1404 <letainkv2001@gmail.com>
2024-11-23 15:21:34 +0700

    update method for sandbox and prod

--- VerificationAPIExample/src/main/java/com/verification/dto/request/VerifyCardRequest.java
+++ VerificationAPIExample/src/main/java/com/verification/dto/request/VerifyCardRequest.java
@@ -13,5 +13,5 @@ public class VerifyCardRequest {
     private String dg13DataB64;
     private String dg14DataB64;
     private String sodData;
-    private String fileId;
+    private String method;
 }
